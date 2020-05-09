dd6cdfe437cef9319abded0ce78e2412a4fbce1a
rjt <rjt.rockx@gmail.com>
2020-05-09 06:04:40 +0530

    Port and implement botclear

---
+++ commands/basic/botClear.js
@@ -0,0 +1,51 @@
+const BaseCommand = require("../../src/base/baseCommand.js");
+
+module.exports = class BotClear extends BaseCommand {
+	constructor(client) {
+		super(client, {
+			name: "botclear",
+			aliases: ["bc"],
+			group: "basic",
+			description: "Clear bot messages as well as messages starting with specific prefixes.",
+			args: [
+				{
+					key: "amount",
+					prompt: "Amount of messages to clear. (Max: 50)",
+					type: "integer",
+					default: 25
+				},
+				{
+					key: "ignorepins",
+					prompt: "Whether to ignore pinned messages or not.",
+					type: "boolean",
+					default: false
+				}
+			],
+			clientPermissions: [
+				"READ_MESSAGE_HISTORY",
+				"MANAGE_MESSAGES"
+			],
+			userPermissions: ["MANAGE_MESSAGES"]
+		});
+	}
+
+	async task(ctx) {
+		if (ctx.args.amount > 50)
+			return ctx.embed({ description: "Cannot clear more than 50 messages at once." });
+		const prefixes = ctx.guildStorage.get("botPrefixes") || ctx.guildStorage.set("botPrefixes", []);
+		try {
+			let messages = await ctx.channel.messages.fetch({ limit: ctx.args.amount, before: ctx.message.id });
+			if (ctx.args.ignorepins)
+				messages = messages.filter(message => !message.pinned);
+			messages = messages.filter(message => message.author.bot || (prefixes.length > 0 && prefixes.some(prefix => message.cleanContent.toLowerCase().startsWith(prefix))));
+			if (messages.length < 1)
+				return ctx.selfDestruct("No bot messages were found.", 5);
+			await ctx.message.delete().catch();
+			await ctx.channel.bulkDelete(messages, true);
+			return ctx.selfDestruct(`Deleted ${messages.size} bot messages in the last ${ctx.args.amount} messages.`, 5);
+		}
+		catch (error) {
+			return ctx.embed({ description: "Unable to delete messages." });
+		}
+	}
+};
---
+++ commands/basic/botClearPrefixes.js
@@ -0,0 +1,80 @@
+const BaseCommand = require("../../src/base/baseCommand.js");
+
+module.exports = class BotClear extends BaseCommand {
+	constructor(client) {
+		super(client, {
+			name: "botclearprefixes",
+			aliases: ["botclearprefix", "bcprefixes", "bcprefix"],
+			group: "basic",
+			description: "Add, remove or list the prefixes configured for botclear.",
+			args: [
+				{
+					key: "action",
+					prompt: "Whether to add/remove/list prefixes",
+					type: "string",
+					oneOf: ["add", "remove", "list"]
+				},
+				{
+					key: "prefix",
+					prompt: "Prefix to add/remove",
+					type: "string",
+					default: ""
+				}
+			],
+			clientPermissions: [
+				"READ_MESSAGE_HISTORY",
+				"MANAGE_MESSAGES"
+			],
+			userPermissions: ["ADMINISTRATOR"]
+		});
+	}
+
+	async task(ctx) {
+		let prefixes = ctx.guildStorage.get("botPrefixes") || ctx.guildStorage.set("botPrefixes", []);
+		if (ctx.args.action === "add") {
+			if (!ctx.args.prefix)
+				return ctx.embed({ description: "Invalid prefix specified." });
+			if (prefixes.includes(ctx.args.prefix.toLowerCase()))
+				return ctx.send({ description: "The given botclear prefix already exists." });
+			prefixes = [...new Set([...prefixes, ctx.args.prefix.toLowerCase()])];
+			ctx.guildStorage.set("botPrefixes", prefixes);
+			return ctx.embed({
+				title: `Prefix ${ctx.args.prefix.toLowerCase()} successfully added.`,
+				fields: [
+					{
+						name: `Current botclear prefixes (${prefixes.length})`,
+						value: prefixes.length > 0 ? prefixes.map(prefix => `\`${prefix}\``).join(", ") : "No botclear prefixes exist."
+					}
+				]
+			});
+		}
+		else if (ctx.args.action === "remove") {
+			if (!ctx.args.prefix)
+				return ctx.embed({ description: "Invalid prefix specified."});
+			if (!prefixes.includes(ctx.args.prefix.toLowerCase()))
+				return ctx.embed({ description: "The given botclear prefix does not exist."});
+			prefixes = prefixes.filter(p => p !== ctx.args.prefix.toLowerCase());
+			prefixes = [...new Set(prefixes)];
+			ctx.guildStorage.set("botPrefixes", prefixes);
+			return ctx.embed({
+				title: `Prefix ${ctx.args.prefix.toLowerCase()} successfully removed.`,
+				fields: [
+					{
+						name: `Current botclear prefixes (${prefixes.length})`,
+						value: prefixes.length > 0 ? prefixes.map(prefix => `\`${prefix}\``).join(", ") : "No botclear prefixes exist."
+					}
+				]
+			});
+		}
+		else if (ctx.args.action === "list") {
+			return ctx.embed({
+				fields: [
+					{
+						name: `Current botclear prefixes (${prefixes.length})`,
+						value: prefixes.length > 0 ? prefixes.map(prefix => `\`${prefix}\``).join(", ") : "No botclear prefixes exist."
+					}
+				]
+			});
+		}
+	}
+};
