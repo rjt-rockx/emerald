51deba3c230be6384d749d043aca020bf47371c1
rjt <rjt.rockx@gmail.com>
2020-05-17 00:04:05 +0530

    Improve field splitting

--- src/paginator.js
+++ src/paginator.js
@@ -1,5 +1,5 @@
 const { MessageEmbed } = require("discord.js");
-const { Util: { splitMessage } } = require("discord.js");
+const { Util: { splitMessage, resolveString } } = require("discord.js");
 
 const chunk = (arrayLike, size) => arrayLike.length === 0 ? [] : [arrayLike.slice(0, size)].concat(chunk(arrayLike.slice(size), size));
 
@@ -8,19 +8,20 @@ module.exports = class Paginator {
 		this.back = "◀";
 		this.next = "▶";
 		this.stop = "⏹";
-		this.timeout = options.timeout && Number.isSafeInteger(options.timeout) && options.timeout <= 300 && options.timeout > 0 ? options.timeout : 15;
 		this.member = ctx.member;
+		this.fields = fields;
 		if (!options) options = {};
+		this.timeout = options.timeout && Number.isSafeInteger(options.timeout) && options.timeout <= 300 && options.timeout > 0 ? options.timeout : 15;
 		if (options.numberFields)
-			fields = fields.map((field, index) => ({ ...field, name: `${index + 1}. ${field.name}` }));
+			this.fields = this.fields.map((field, index) => ({ ...field, name: `${index + 1}. ${field.name}` }));
 		if (options.splitLongFields)
-			fields = this.splitLongFields(fields);
+			this.fields = this.splitLongFields(this.fields);
 		if (typeof options.chunkSize !== "number" || options.chunkSize < 1 || options.chunkSize > 12)
 			options.chunkSize = 5;
 		if (typeof options.defaultPage !== "number" || !(options.defaultPage >= 0 && options.defaultPage <= fields.length))
 			options.defaultPage = 0;
 		this.current = options.defaultPage;
-		this.fields = chunk(fields, options.chunkSize);
+		this.fields = chunk(this.fields, options.chunkSize);
 		this.total = this.fields.length;
 		this.embedTemplate = typeof options.embedTemplate === "object" ? options.embedTemplate : {};
 
@@ -67,24 +68,49 @@ module.exports = class Paginator {
 		});
 	}
 
-	splitLongFields(fields) {
-		const longFields = fields.map((field, index) => ({ ...field, long: field.value.length > 1024, index })).filter(field => field.long && field.index);
-		for (const longField of longFields) {
-			let splitFields = splitMessage(longField.value, { maxLength: 1024 }).map(value => ({ name: longField.name, value }));
-			if (splitFields.length === 2)
-				splitFields[1].name = (splitFields[1].name > 256 ? splitFields[1].name.substring(0, 244) : splitFields[1].name) + "... (contd.)";
-			else if (splitFields.length > 2) {
-				splitFields = splitFields.map((field, splitIndex) => {
-					if (splitIndex) {
-						const continueString = `... (contd.${splitIndex > 1 ? ` - ${splitIndex}` : ""})`;
-						field.name = (field.name > 256 ? field.name.substring(0, 256 - continueString.length) : splitFields[1].name) + continueString;
-					}
-					return field;
-				});
+	splitText(text, { maxLength = 2000, characters = ["\n", " "], prepend = "", append = "" }) {
+		if (!Array.isArray(characters))
+			if (characters instanceof String)
+				characters = [characters];
+		for (const char of characters) {
+			try {
+				const result = splitMessage(text, { maxLength, char, prepend, append });
+				return result;
 			}
-			fields = fields.splice(longField.index, 1, ...splitFields);
+			catch (err) {
+				if (err instanceof RangeError) continue;
+				break;
+			}
 		}
-		return fields;
+		const resolvedText = resolveString(text);
+		const splits = resolvedText.match(new RegExp(`.{1,${maxLength - (prepend.length + append.length)}}`, "g"));
+		return splits.map(t => t.trim()).filter(t => !!t).map(t => prepend + t + append);
+	}
+
+	splitLongFields(fields, lengths = { name: 256, value: 1024 }) {
+		let newFields = fields;
+		const longFields = newFields.map((field, index) => ({ ...field, long: field.value.length > lengths.value, index })).filter(field => field.long && (typeof field.index !== "undefined"));
+		for (const longField of longFields) {
+			const splitFields = this.splitText(longField.value, { maxLength: lengths.value }).map(value => ({ name: longField.name, value, inline: !!longField.inline }));
+			newFields.splice(longField.index, 1, ...splitFields);
+		}
+		if (newFields.length && newFields.length < 2) {
+			newFields[0].name = (newFields[0].name.length > lengths.name ? newFields[0].name.substring(0, lengths.name - 3) : newFields[0].name) + "...";
+		}
+		else if (newFields.length === 2) {
+			const continueString = "... (contd.)";
+			newFields[1].name = (newFields[1].name.length > lengths.name ? newFields[1].name.substring(0, lengths.name - continueString.length) : newFields[1].name) + "... (contd.)";
+		}
+		else if (newFields.length > 2) {
+			newFields = newFields.map((field, splitIndex) => {
+				if (splitIndex) {
+					const continueString = `... (contd.${splitIndex > 1 ? ` - ${splitIndex}` : ""})`;
+					field.name = (field.name > lengths.name ? field.name.substring(0, lengths.name - continueString.length) : field.name) + continueString;
+				}
+				return field;
+			});
+		}
+		return newFields;
 	}
 
 	refresh() {
