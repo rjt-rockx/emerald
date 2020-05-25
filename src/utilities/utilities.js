414fa38addfaeaf97aa1092d0c6221f99f7e7f28
rjt <rjt.rockx@gmail.com>
2020-05-25 15:26:27 +0530

    Add DiscordColors to utilities

--- src/utilities/utilities.js
+++ src/utilities/utilities.js
@@ -5,6 +5,16 @@ const deepFunctions = x => deepProps(x).filter(name => typeof x[name] === "funct
 const userFunctions = x => [...new Set(deepFunctions(x).filter(name => name !== "constructor" && !~name.indexOf("_")))];
 const toTitleCase = str => str.replace(/\w\S*/g, txt => txt.charAt(0).toUpperCase() + txt.substr(1));
 const properRoundToTwo = num => +(Math.round(num + "e+2") + "e-2");
-const newlinePattern = new RegExp("!!NL!!", "g");
+const DiscordColors = {
+	RED: 0xF04747,
+	GREEN: 0x43B581,
+	YELLOW: 0xFAA61A,
+	BLUE: 0x00B0F4,
+	PURPLE: 0x593695,
+	BLURPLE: 0x7289DA,
+	GREY: 0x747F8D,
+	DARK_GREY: 0x2C2F33,
+	DARKER_GREY: 0x23272A
+};
 
-module.exports = { escapeRegex, deepProps, deepFunctions, userFunctions, toTitleCase, newlinePattern };
+module.exports = { escapeRegex, deepProps, deepFunctions, userFunctions, toTitleCase, properRoundToTwo, DiscordColors };
