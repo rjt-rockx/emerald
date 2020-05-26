bca4edd8a64bf11cd4be52bfafaa296839511272
rjt <rjt.rockx@gmail.com>
2020-05-26 06:04:51 +0530

    Add proper error message to prefix length check

--- commands/basic/prefix.js
+++ commands/basic/prefix.js
@@ -13,8 +13,7 @@ module.exports = class Prefix extends BaseCommand {
 					key: "prefix",
 					prompt: "New prefix to set, if any.",
 					type: "string",
-					default: "",
-					validate: v => String(v).length <= 16
+					default: ""
 				}
 			]
 		});
@@ -24,6 +23,8 @@ module.exports = class Prefix extends BaseCommand {
 		const currentPrefix = ctx.guildStorage.get("commandPrefix");
 		if (!ctx.args.prefix)
 			return ctx.embed({ title: "Guild prefix", description: `Command prefix for this guild is currently set to \`${currentPrefix}\`` });
+		if (ctx.args.prefix.length > 16)
+			return ctx.embed({ description: "Prefix must be less than 16 characters long." });
 		ctx.guild.commandPrefix = ctx.args.prefix;
 		ctx.guildStorage.set("commandPrefix", ctx.args.prefix);
 		return ctx.embed({ title: "Guild prefix", description: `Command prefix for this guild successfully set to \`${ctx.args.prefix}\`` });
