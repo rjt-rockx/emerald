a47f2b543277a385fc34a97717368b1eb07939a4
rjt <rjt.rockx@gmail.com>
2020-07-16 19:52:15 +0530

    Add initial Audit Log support

--- src/base/baseContext.js
+++ src/base/baseContext.js
@@ -2,8 +2,47 @@ const { MessageEmbed, SnowflakeUtil } = require("discord.js");
 const logger = require("../utilities/logger.js");
 const paginator = require("../utilities/paginator.js");
 const { idSort, byText } = require("../utilities/utilities.js");
+const diff = require("../utilities/diffUtils.js");
 const dataHandler = require("../handlers/dataHandler.js");
 
+const AuditLogEvents = {
+	all: null,
+	guildUpdate: 1,
+	channelCreate: 10,
+	channelUpdate: 11,
+	channelDelete: 12,
+	channelOverwriteCreate: 13,
+	channelOverwriteUpdate: 14,
+	channelOverwriteDelete: 15,
+	memberKick: 20,
+	memberPrune: 21,
+	guildBanAdd: 22,
+	guildBanRemove: 23,
+	guildMemberUpdate: 24,
+	memberRoleUpdate: 25,
+	memberMove: 26,
+	memberDisconnect: 27,
+	botAdd: 28,
+	roleCreate: 30,
+	roleUpdate: 31,
+	roleDelete: 32,
+	inviteCreate: 40,
+	inviteUpdate: 41,
+	inviteDelete: 42,
+	webhookCreate: 50,
+	webhookUpdate: 51,
+	webhookDelete: 52,
+	emojiCreate: 60,
+	emojiUpdate: 61,
+	emojiDelete: 62,
+	messageDelete: 72,
+	messageDeleteBulk: 73,
+	messagePin: 74,
+	messageUnpin: 75,
+	integrationCreate: 80,
+	integrationUpdate: 81,
+	integrationDelete: 82
+};
 module.exports = class Context {
 	constructor(client, event) {
 		this.client = client;
@@ -23,6 +62,176 @@ module.exports = class Context {
 			return Promise.resolve(this[prop]);
 		}));
 		return this;
+	}
+
+	async fetchAuditLog() {
+		if (!this.guild) return;
+		const hasAuditLogPermission = this.guild.member(this.client.user).hasPermission("VIEW_AUDIT_LOG");
+		if (!hasAuditLogPermission)
+			return Promise.reject("No audit log permission.");
+
+		let types = [];
+		if (Object.keys(AuditLogEvents).includes(this.event))
+			types = [this.event];
+		else if (this.event === "channelPinsUpdate")
+			types = ["messagePin", "messageUnpin"];
+		else if (this.event === "guildMemberAdd" && this.member.user.bot)
+			types = ["botAdd"];
+		else if (this.event === "guildMemberAdd" && !this.member.user.bot)
+			types = ["inviteUpdate"];
+		else if (this.event === "guildMemberRemove")
+			types = ["memberKick", "memberPrune"];
+		if (this.event === "guildMemberUpdate")
+			types.push("memberRoleUpdate");
+		if (this.event === "webhookUpdate")
+			types.push("webhookCreate", "webhookDelete");
+		types = [...new Set(types)];
+		const entries = [];
+		for (const type of types) {
+			const fetchedAuditLogs = await this.guild.fetchAuditLogs({
+				type: AuditLogEvents[type],
+				limit: 2,
+				before: SnowflakeUtil.generate(this.emittedAt.getTime() + 2000)
+			}).catch(() => { });
+			if (!fetchedAuditLogs || !fetchedAuditLogs.entries) break;
+			entries.push(...fetchedAuditLogs.entries.values());
+		}
+		this._assignMatchingAuditLogEntry(entries.sort(idSort).reverse(), types);
+		return this;
+	}
+
+	_assignMatchingAuditLogEntry(entries, types) {
+		let matchingEntry;
+		// console.log("entries:\n" + entries.map(e => JSON.stringify(e, null, 2)).join("\n"));
+		console.log("types:" + types);
+		if (["channelCreate", "channelDelete", "channelUpdate"].includes(this.event)) {
+			matchingEntry = entries.find(e => (typeof e.target === "string" ? e.target : e.target.id) === this.channel.id);
+			if (matchingEntry) {
+				this.auditLogEntry = matchingEntry;
+				this[byText(matchingEntry.actionType)] = matchingEntry.executor;
+			}
+		}
+		else if (["roleCreate", "roleDelete", "roleUpdate"].includes(this.event)) {
+			matchingEntry = entries.find(e => (typeof e.target === "string" ? e.target : e.target.id) === this.role.id);
+			if (matchingEntry) {
+				this.auditLogEntry = matchingEntry;
+				this[byText(matchingEntry.actionType)] = matchingEntry.executor;
+			}
+		}
+		else if (["emojiCreate", "emojiDelete", "emojiUpdate"].includes(this.event)) {
+			matchingEntry = entries.find(e => (typeof e.target === "string" ? e.target : e.target.id) === this.emoji.id);
+			if (matchingEntry) {
+				this.auditLogEntry = matchingEntry;
+				this[byText(matchingEntry.actionType)] = matchingEntry.executor;
+			}
+		}
+		else if (["guildMemberAdd"].includes(this.event)) {
+			matchingEntry = entries.find(e => (e.target.id === this.member.id) && this.member.user.bot);
+			if (matchingEntry) {
+				this.auditLogEntry = matchingEntry;
+				this.addedBy = matchingEntry.executor;
+			}
+		}
+		else if (["guildMemberRemove"].includes(this.event)) {
+			matchingEntry = entries.find(e => (e.target.id === this.member.id) && this.member.user.bot);
+			if (matchingEntry) {
+				this.auditLogEntry = matchingEntry;
+				const executorText = matchingEntry.action === "MEMBER_KICK" ? "kickedBy" : "prunedBy";
+				this[executorText] = matchingEntry.executor;
+			}
+		}
+		else if (["guildBanAdd"].includes(this.event)) {
+			matchingEntry = entries.find(e => e.target.id === this.user.id);
+			if (matchingEntry) {
+				this.auditLogEntry = matchingEntry;
+				this.bannedBy = matchingEntry.executor;
+			}
+		}
+		else if (["guildBanRemove"].includes(this.event)) {
+			matchingEntry = entries.find(e => e.target.id === this.user.id);
+			if (matchingEntry) {
+				this.auditLogEntry = matchingEntry;
+				this.unbannedBy = matchingEntry.executor;
+			}
+		}
+		else if (["guildMemberUpdate"].includes(this.event)) {
+			matchingEntry = entries.find(e => (typeof e.target === "string" ? e.target : e.target.id) === this.user.id);
+			if (matchingEntry) {
+				this.auditLogEntry = matchingEntry;
+				this.updatedBy = matchingEntry.executor;
+			}
+		}
+		else if (["inviteCreate", "inviteDelete", "inviteUpdate"].includes(this.event)) {
+			matchingEntry = entries.find(e => (typeof e.target === "string" ? e.target : e.target.id) === this.invite.id);
+			if (matchingEntry) {
+				this.auditLogEntry = matchingEntry;
+				this[byText(matchingEntry.actionType)] = matchingEntry.executor;
+			}
+		}
+		else if (["messageDelete"].includes(this.event)) {
+			matchingEntry = entries.find(e => e.extra.channel.id === this.message.channel.id);
+			if (matchingEntry) {
+				this.auditLogEntry = matchingEntry;
+				this.deletedBy = matchingEntry.executor;
+			}
+		}
+		else if (["messageDeleteBulk"].includes(this.event)) {
+			matchingEntry = entries.find(e => (typeof e.target === "string" ? e.target : e.target.id) === this.channel.id);
+			if (matchingEntry) {
+				this.auditLogEntry = matchingEntry;
+				this.deletedBy = matchingEntry.executor;
+			}
+		}
+		else if (["channelPinsUpdate"].includes(this.event)) {
+			matchingEntry = entries.find(e => e.extra.channel.id === this.channel.id);
+			if (matchingEntry) {
+				this.auditLogEntry = matchingEntry;
+				const executorText = matchingEntry.action === "MESSAGE_PIN" ? "pinnedBy" : "unpinnedBy";
+				this[executorText] = matchingEntry.executor;
+			}
+		}
+		else if (["guildUpdate"].includes(this.event)) {
+			[matchingEntry] = entries;
+			this.auditLogEntry = matchingEntry;
+			this.updatedBy = matchingEntry.executor;
+		}
+		else if (["webhookUpdate"].includes(this.event)) {
+			console.log(entries);
+			matchingEntry = entries.find(e => e.target.channelID === this.channel.id);
+			this.auditLogEntry = matchingEntry;
+			this[byText(matchingEntry.actionType)] = matchingEntry.executor;
+		}
+	}
+
+	get isUpdateEvent() {
+		const updateEvents = ["guildUpdate", "channelUpdate", "emojiUpdate", "guildMemberUpdate", "voiceStateUpdate", "presenceUpdate", "messageUpdate", "roleUpdate", "userUpdate"];
+		return updateEvents.includes(this.event);
+	}
+
+	get changeCount() {
+		return this.changes ? Object.keys(this.changes).length : 0;
+	}
+
+	get changes() {
+		if (!this.isUpdateEvent) return null;
+		else if (this.isUpdateEvent && this._changes)
+			return Object.keys(this._changes).length ? this._changes : null;
+		else if (this.event === "guildUpdate")
+			this._changes = diff(this.oldGuild, this.newGuild);
+		else if (this.event === "channelUpdate")
+			this._changes = diff(this.oldChannel, this.newChannel);
+		else if (this.event === "emojiUpdate")
+			this._changes = diff(this.oldEmoji, this.newEmoji);
+		else if (["guildMemberUpdate", "voiceStateUpdate", "presenceUpdate"].includes(this.event))
+			this._changes = diff(this.oldMember, this.newMember);
+		else if (this.event === "messageUpdate")
+			this._changes = diff(this.oldMessage, this.newMessage);
+		else if (this.event === "roleUpdate")
+			this._changes = diff(this.oldRole, this.newRole);
+		else if (this.event === "userUpdate") {
+			this._changes = diff(this.oldUser, this.newUser);
+		}
+		return Object.keys(this._changes).length ? this._changes : null;
 	}
 
 	get services() {
