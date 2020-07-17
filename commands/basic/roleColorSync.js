961430e5e08676748e52a6adf628b5d3d3cf8164
rjt <rjt.rockx@gmail.com>
2020-07-17 23:51:36 +0530

    Rename sync role color command, sync on invoke

--- commands/basic/roleColorSync.js
+++ commands/basic/roleColorSync.js
@@ -1,10 +1,10 @@
 const BaseCommand = require("../../src/base/baseCommand.js");
 
-module.exports = class SyncRoleColor extends BaseCommand {
+module.exports = class RoleColorSync extends BaseCommand {
 	constructor(client) {
 		super(client, {
-			name: "syncrolecolor",
-			aliases: ["src"],
+			name: "rolecolorsync",
+			aliases: ["rcs"],
 			group: "basic",
 			description: "Sync a role color to a user's avatar.",
 			clientPermissions: ["SEND_MESSAGES", "EMBED_LINKS", "MANAGE_ROLES"],
@@ -36,13 +36,16 @@ module.exports = class SyncRoleColor extends BaseCommand {
 		if (ctx.args.action === "enable") {
 			if (!ctx.args.user)
 				return ctx.embed("No user specified.");
+			if (!ctx.args.role.editable)
+				return ctx.embed("Role is not editable by the bot.");
+			await ctx.services.rolesync.updateRole(ctx.args.role, ctx.args.user);
 			syncedRoles[ctx.args.role.id] = ctx.args.user.id;
 			ctx.guildStorage.set("syncedRoles", syncedRoles);
 			return ctx.embed(`Role color of ${ctx.args.role.name} will be synced to ${ctx.args.user}'s avatar from now on.`);
 		}
 		else if (ctx.args.action === "disable") {
-			if (!Object.keys(syncedRoles).has(ctx.args.role.id))
-				return ctx.embed(`No role color sync set for ${ctx.args.role.name}`);
+			if (!Object.keys(syncedRoles).includes(ctx.args.role.id))
+				return ctx.embed(`No role color sync set for the ${ctx.args.role.name} role.`);
 			delete syncedRoles[ctx.args.role.id];
 			ctx.guildStorage.set("syncedRoles", syncedRoles);
 			return ctx.embed(`Role color of ${ctx.args.role.name} will no longer be synced from now on.`);
