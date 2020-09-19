6e319afef650fba8689aeac4ff04b64ce1ef1004
rjt <rjt.rockx@gmail.com>
2020-09-19 12:51:19 +0530

    Port ArtChannel service as ReactChannel

---
+++ commands/misc/reactchannel.js
@@ -0,0 +1,58 @@
+const BaseCommand = require("../../src/base/baseCommand.js");
+const reactionMap = {
+	like: ["👍"],
+	dislike: ["👎"],
+	likedislike: ["👍", "👎"],
+	upvote: ["🔺"],
+	downvote: ["🔻"],
+	upvotedownvote: ["🔺", "🔻"]
+};
+
+module.exports = class ReactChannel extends BaseCommand {
+	constructor(client) {
+		super(client, {
+			name: "reactchannel",
+			description: "Add or remove a react channel. Messages containing a link or embed will be reacted with a vote emoji in this channel.",
+			group: "misc",
+			memberName: "reactchannel",
+			userPermissions: ["ADMINISTRATOR"],
+			clientPermissions: ["SEND_MESSAGES", "EMBED_LINKS"],
+			args: [
+				{
+					key: "enabled",
+					prompt: "Whether to enable or disable reactchannel.",
+					type: "string",
+					oneOf: ["enable", "disable"]
+				},
+				{
+					key: "channel",
+					prompt: "Channel to react in.",
+					type: "channel"
+				},
+				{
+					key: "reactionType",
+					prompt: "One of the folowing:\nLike => 👍\nDislike =>👎\nLikeDislike => 👍,👎\nUpvote => 🔺\nDownvote => 🔻\nUpvoteDownvote => 🔺,🔻",
+					type: "string",
+					oneOf: ["like", "likedislike", "dislike", "upvote", "downvote", "upvotedownvote"],
+					default: "upvote"
+				}
+			]
+		});
+	}
+
+	async task(ctx) {
+		let reactchannels = ctx.globalStorage.get("reactchannels") || ctx.globalStorage.set("reactchannels", []);
+		if (ctx.args.enabled === "enable") {
+			if (reactchannels.map(channel => channel.id).includes(ctx.args.channel.id))
+				reactchannels = reactchannels.filter(channel => channel.id !== ctx.args.channel.id);
+			reactchannels.push({
+				id: ctx.args.channel.id,
+				reactions: typeof reactionMap[ctx.args.reactionType.toLowerCase()] === "undefined" ? reactionMap.upvote : reactionMap[ctx.args.reactionType.toLowerCase()]
+			});
+		}
+		else if (ctx.args.enabled === "disable")
+			reactchannels = reactchannels.filter(channel => channel.id !== ctx.args.channel.id);
+		ctx.globalStorage.set("reactchannels", reactchannels);
+		return ctx.embed(`React Channel successfully ${ctx.args.enabled === "enable" ? "set to" : "removed from"} #${ctx.args.channel.name}`);
+	}
+};
---
+++ services/ReactChannel.js
@@ -0,0 +1,25 @@
+const urlRegex = require("url-regex-local")({ exact: false, strict: false });
+const BaseService = require("../src/base/baseService.js");
+
+module.exports = class ReactChannel extends BaseService {
+	constructor(client) {
+		super(client, {
+			name: "React Channel Service",
+			description: "Adds upvote/downvote/like/dislike reactions on messages containing a link, embed or attachment.",
+			enabled: true,
+			fetchPartials: true
+		});
+	}
+
+	async onMessage(ctx) {
+		if (ctx.message.author.bot) return;
+		const reactchannels = ctx.globalStorage.get("reactchannels");
+		if (!reactchannels) return ctx.globalStorage.set("reactchannels", []);
+		const index = reactchannels.map(channel => channel.id).indexOf(ctx.channel.id);
+		if (index > -1) {
+			// if (ctx.message.attachments.size > 0 || ctx.message.embeds.length > 0 || urlRegex.test(ctx.message.cleanContent))
+			for (const reaction of reactchannels[index].reactions)
+				await ctx.message.react(reaction);
+		}
+	}
+};
