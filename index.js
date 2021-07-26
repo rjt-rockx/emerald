768bbab35c84757a343477abd02239cdb9fcffd9
rjt <rjt.rockx@gmail.com>
2021-07-26 21:10:30 +0530

    Update Intents

--- index.js
+++ index.js
@@ -15,8 +15,18 @@ const client = new CommandoClient({
 	commandPrefix: config.defaultPrefix,
 	nonCommandEditable: false,
 	commandEditableDuration: 0,
-	partials: ["CHANNEL", "MESSAGE", "REACTION"], 
-	ws: { intents: Intents.ALL }
+	partials: ["CHANNEL", "MESSAGE", "REACTION"],
+	intents: [
+		Intents.FLAGS.GUILDS,
+		Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS,
+		Intents.FLAGS.GUILD_MESSAGES,
+		Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+		Intents.FLAGS.GUILD_WEBHOOKS,
+		Intents.FLAGS.GUILD_INVITES,
+		Intents.FLAGS.GUILD_BANS,
+		Intents.FLAGS.GUILD_MEMBERS,
+		Intents.FLAGS.GUILD_PRESENCES
+	]
 });
 
 client.once("ready", async () => {
